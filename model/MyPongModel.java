eabe511ccf02e35be70ba7d6d9f38ac010a3814d
Erik Englund <erikw.englund@gmail.com>
2015-01-02 18:13:27 +0100

    added our version of model

---
+++ model/MyPongModel.java
@@ -0,0 +1,27 @@
+package model;
+
+import controller.*;
+import view.PongView;
+
+import java.awt.*;
+import java.util.Set;
+
+/**
+ * Created by ErikWE on 02/01/15.
+ */
+public class MyPongModel implements PongModel{
+    
+    public MyPongModel(String p1, String p2){};
+    
+    
+    /**
+     * getters that take a BarKey LEFT or RIGHT
+     * and return positions of the various items on the board
+     */
+    public int getBarPos(BarKey k){
+        
+        
+    };
+    public int getBarHeight(BarKey k);
+    public Point getBallPos();
+}
