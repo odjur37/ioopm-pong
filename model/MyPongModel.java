702498700ba0b0e7f6863860c82a315f33f2a870
Erik Englund <erikw.englund@gmail.com>
2015-01-03 20:34:47 +0100

    messages are now printed according to plan

--- model/MyPongModel.java
+++ model/MyPongModel.java
@@ -21,55 +21,68 @@ public class MyPongModel implements PongModel{
     private int ballPosX = 500;
     private int ballPosY = 500;
     private Point ballPos = new Point(ballPosX,ballPosY);
-    private String message = "START!";
+    private String message;
     private String scoreLeft = "0";
     private String scoreRight = "0";
     private int ballSpeed = 10;
     private Dimension fieldSize = new Dimension(1000, 1000);
     
-    public void compute(Set<Input> input, long delta_t){
-        for (Input eachSet : input) {
-            if (eachSet.key.equals(BarKey.LEFT)) {
-                if (eachSet.dir.equals(Input.Dir.UP)) {
-                    this.barPosLeft -= 5;
-                } else {
-                    this.barPosLeft += 5;
+    public void compute(Set<Input> input, long delta_t) {
+        Integer scoreLeftInt = Integer.parseInt(scoreLeft);
+        Integer scoreRightInt = Integer.parseInt(scoreRight);
+        if (scoreLeftInt.equals(10)) {
+            setMessage(leftPlayerName + " wins!!");
+        }
+        if (scoreRightInt.equals(10)) {
+            setMessage(rightPlayerName + " wins!!");
+        } else {
+            for (Input eachSet : input) {
+                if (eachSet.key.equals(BarKey.LEFT)) {
+                    if (eachSet.dir.equals(Input.Dir.UP)) {
+                        this.barPosLeft -= 5;
+                    } else {
+                        this.barPosLeft += 5;
+                    }
+                }
+                if (eachSet.key.equals(BarKey.RIGHT)) {
+                    if (eachSet.dir.equals(Input.Dir.UP)) {
+                        this.barPosRight -= 5;
+                    } else {
+                        this.barPosRight += 5;
+                    }
                 }
             }
-            if (eachSet.key.equals(BarKey.RIGHT)) {
-                if (eachSet.dir.equals(Input.Dir.UP)) {
-                    this.barPosRight -= 5;
+            if (ballPosX == 980) {
+                if (((ballPosY - 10) <= barPosRight + (barHeightRight / 2)) && ((ballPosY + 10) >= barPosRight - (barHeightRight / 2))) {
+                    this.ballSpeed = ((this.ballSpeed) * -1);
+                    setMessage(null);
                 } else {
-                    this.barPosRight += 5;
+                    Integer tempScore = Integer.parseInt(scoreLeft) + 1;
+                    this.scoreLeft = Integer.toString(tempScore);
+                    ballPosX = 500;
+                    ballPosY = 500;
+                    ballPos = new Point(ballPosX, ballPosY);
+                    barPosRight = 500;
+                    setMessage(leftPlayerName + " scores!");
                 }
             }
-        }
-        if(ballPosX == 980){
-            if (((ballPosY-10) <= barPosRight+(barHeightRight/2)) && ((ballPosY+10) >= barPosRight-(barHeightRight/2))) {
-                this.ballSpeed = ((this.ballSpeed) * -1);
-            }else{
-                Integer tempScore = Integer.parseInt(scoreLeft) + 1;
-                this.scoreLeft = Integer.toString(tempScore);
-                ballPosX = 500;
-                ballPosY = 500;
-                ballPos = new Point(ballPosX,ballPosY);
-                barPosRight = 500;
+            if (ballPosX == 20) {
+                if (((ballPosY - 10) <= barPosLeft + (barHeightLeft / 2)) && ((ballPosY + 10) >= barPosLeft - (barHeightLeft / 2))) {
+                    this.ballSpeed = ((this.ballSpeed) * -1);
+                    setMessage(null);
+                } else {
+                    Integer tempScore = Integer.parseInt(scoreRight) + 1;
+                    this.scoreRight = Integer.toString(tempScore);
+                    ballPosX = 500;
+                    ballPosY = 500;
+                    ballPos = new Point(ballPosX, ballPosY);
+                    barPosLeft = 500;
+                    setMessage(rightPlayerName + " scores!");
+                }
             }
+            this.ballPosX += ballSpeed;
+            this.ballPos = new Point(ballPosX, ballPosY);
         }
-        if(ballPosX == 20) {
-            if (((ballPosY-10) <= barPosLeft+(barHeightLeft/2)) && ((ballPosY+10) >= barPosLeft-(barHeightLeft/2))) {
-                this.ballSpeed = ((this.ballSpeed) * -1);
-            }else{
-                Integer tempScore = Integer.parseInt(scoreRight) + 1;
-                this.scoreRight = Integer.toString(tempScore);
-                ballPosX = 500;
-                ballPosY = 500;
-                ballPos = new Point(ballPosX,ballPosY);
-                barPosLeft = 500;
-            }
-        }
-        this.ballPosX += ballSpeed;
-        this.ballPos = new Point(ballPosX,ballPosY);
     }
     
     public MyPongModel(String p1, String p2){
@@ -94,7 +107,10 @@ public class MyPongModel implements PongModel{
     public Point getBallPos(){
         return this.ballPos;
     }
-
+    
+    public void setMessage(String msg){
+        this.message = msg;
+    }
     public String getMessage(){
         return this.message;
     }
