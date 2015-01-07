a4b6da1112629c0438ec83f2c39d7d53947982d9
Erik Englund <erikw.englund@gmail.com>
2015-01-07 13:21:16 +0100

    changed curveCOunt

--- model/MyPongModel.java
+++ model/MyPongModel.java
@@ -122,19 +122,19 @@ public class MyPongModel implements PongModel{
         for (Input eachSet : input) {
             if (eachSet.key.equals(BarKey.LEFT)) {
                 if (eachSet.dir.equals(Input.Dir.UP)) {
-                    this.curveBallCount = 100;
+                    this.curveBallCount = 50;
                     this.curveBallDirection = "DOWN";
                 } else if (eachSet.dir.equals(Input.Dir.DOWN)) {
-                    this.curveBallCount = 100;
+                    this.curveBallCount = 50;
                     this.curveBallDirection = "UP";
                 }
             }
             if (eachSet.key.equals(BarKey.RIGHT)) {
                 if (eachSet.dir.equals(Input.Dir.UP)) {
-                    this.curveBallCount = 100;
+                    this.curveBallCount = 50;
                     this.curveBallDirection = "DOWN";
                 } else if (eachSet.dir.equals(Input.Dir.DOWN)) {
-                    this.curveBallCount = 100;
+                    this.curveBallCount = 50;
                     this.curveBallDirection = "UP";
                 }
             }
@@ -178,7 +178,7 @@ public class MyPongModel implements PongModel{
      * @param input A Set-array containing user-input.
      */
     public void barHitCompute(Set<Input> input) {
-        //decideCurveHit(input);
+        decideCurveHit(input);
         decideCornerHit();
         if (barHitCount == 0) {
             this.ballSpeedY = this.ballSpeedY * 1.07;
